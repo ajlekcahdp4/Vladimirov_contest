edd2f8f29ea89f338c1a43c2a948506447f2cca8
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-03-11 15:46:44 +0300

    generating small tests

--- HWH/LVL3/tests/TestGen.py
+++ HWH/LVL3/tests/TestGen.py
@@ -1,46 +1,81 @@
 import random as rand
 
-src = open ("tests/test.src", "r")
-src2 = open ("tests/test2.src", "r")
-dest = open ("tests/4.dat", "w")
+def FileToBuf (filename, buf):
+    file = open (filename, "r")
+    N = int(file.readline())
 
-N = int(src.readline())
-N = 10
-strings = []
-text = []
-src2_buf = []
+    for i in range (0, N):
+        buf.append((file.readline()).strip())
 
-for i in range (0, N):
-    strings.append((src.readline()).strip())
-for i in range (0, N):
-    src2_buf.append((src2.readline()).strip())
+    file.close()
+    return N
 
-src.close()
-for i in range (0, N):
-    str_len = len (strings[i])
-    l1 = rand.randint (3, str_len - 2)
-    text.append (strings[i][l1:str_len])
-    text.append (strings[i][:l1])
+def CrushingWords (buf, N):
+    text = []
+    for i in range (0, N):
+        str_len = len (buf[i])
+        l1 = rand.randint (3, str_len - 2)
+        text.append (buf[i][l1:str_len])
+        text.append (buf[i][:l1])
+        
+        l2 = l1
+        while (l2 == l1):
+            l2 = rand.randint (3, str_len - 2)
+        text.append (buf[i][l2:str_len])
+        text.append (buf[i][:l2])
+    return text
+
+def GenerateTest (words, N1, text2, N2):
+    test = []
+    words_index_list = []
+
+    test_len = rand.randint (3, N1)
+    answ = rand.randint (3, 100)
+
+    print ("test_len = " + str(4 * test_len))
+    print ("answ = %s"%(answ))
+    test.append (str(answ * 4 + N2))
+    #insert fours
+    for cnt in range(answ):
+        i = 1
+        while (i % 4 != 0 or words_index_list.count(i) > 0):
+            i = rand.randint (0, N1 - 4)
+        words_index_list.append (i)
+        test.append (words[i])
+        test.append (words[i + 1])
+        test.append (words[i + 2])
+        test.append (words[i + 3])
+    #insert other words
+    words_index_list = []
+    for i in range (0, N2):
+        index = 0
+        while (words_index_list.count(index) > 0):
+            index = rand.randint (0, N2 - 1)
+        words_index_list.append (index)
+        test.append (text2[index])
+
+
+    return test
     
-    l2 = l1
+#=================================================================
+#===============================MAIN==============================
+#=================================================================
 
-    while (l2 == l1):
-        l2 = rand.randint (3, str_len - 2)
-    text.append (strings[i][l2:str_len])
-    text.append (strings[i][:l2])
-    i += 1
+first_text = []
+second_text = []
 
+N1 = FileToBuf ("tests/src/1.src", first_text)
+N2 = FileToBuf ("tests/src/2.src", second_text)
 
+test_file = open ("tests/dat/4.dat", "w")
 
-N1 = rand.randint (3, N)
-print (str(2*N1))
-dest.write (str(2 * N1) + '\n')
-
-for i in range (0, N1):
-    dest.write (text[i] + '\n')
-    l = rand.randint (N1 // 2, len(src2_buf) - 1)
-    dest.write (src2_buf[l] + '\n')
-
-
-dest.close()
-src2.close()
+#------------------------------------------------------------------
+words = CrushingWords (first_text, N1)
+#------------------------------------------------------------------
+test = GenerateTest (words, N1 * 4, second_text, N2)
+#------------------------------------------------------------------
+test_len = len (test)
+for i in range (0, test_len):
+    test_file.write (test[i] + '\n')
+#------------------------------------------------------------------
+test_file.close()
