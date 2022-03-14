27c589c841b553901c8c535d290bb5668e271369
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-03-14 10:46:30 +0300

    ListInit and ListCount

--- HWH/LVL3/hashtable/hashtable.c
+++ HWH/LVL3/hashtable/hashtable.c
@@ -41,7 +41,21 @@ struct node *ListInit (void)
     return top;
 }
 //==================================================================================
+size_t ListCount (struct node *top, struct node *aim_node)
+{
+    assert (top);
+    assert (aim_node);
 
+    size_t cnt = 0;
+    struct node *cur = top;
+    while (cur)
+    {
+        if (cur != top && nodecmp (cur, aim_node) == 0)
+            cnt += 1;
+        cur = cur->next;
+    }
+    return cnt;
+}
 //==================================================================================
 
 struct Hashtable* HashTableInit (size_t size, unsigned long long (*hash_f)(const char*))
