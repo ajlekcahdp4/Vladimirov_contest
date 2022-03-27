686172a2b3920a49fc29c26d44db70bdcb33af59
ajlekcahdp4 <alex.rom23@mail.ru>
2022-03-27 13:51:48 +0300

    graph dump

---
+++ lexer_and_parser/dump_tree/dump_tree.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dump_tree.h"
+
+enum lexem_kind_t { OP, BRACE, NUM };
+enum operation_t { ADD, SUB, MUL, DIV };
+enum braces_t { LBRAC, RBRAC };
+
+struct lexem_t {
+    enum lexem_kind_t kind;
+    union {
+        enum operation_t op;
+        enum braces_t b;
+        int num;
+    } lex;
+};
+
+
+struct node_t {
+  struct node_t *left, *right;
+  struct lexem_t data;
+};
+
+
+
+
+
+void DtStart (FILE* dotfile)
+{
+    fprintf (dotfile, "digraph G {\n");
+    fprintf (dotfile, "rankdir = LR\n");
+}
+
+void DtSetNode (FILE* dotfile, struct node_t *node)
+{
+    fprintf (dotfile, "Node%p [style=filled, fillcolor = \"palegreen\" ,label=\"", node);
+    printf ("kind = %d, union = %d\n", node->data.kind, node->data.lex.num);
+    switch (node->data.kind)
+    {
+        case OP:
+            switch (node->data.lex.op)
+            {
+                case ADD:
+                    fprintf (dotfile, "+");
+                    break;
+                case SUB:
+                    fprintf (dotfile, "-");
+                    break;
+                case MUL:
+                    fprintf (dotfile, "*");
+                    break;
+                case DIV:
+                    fprintf (dotfile, "/");
+                    break;
+                default:
+                    printf ("ERROR\n");
+                    break;
+            }
+            break;
+        case NUM:
+            fprintf (dotfile, "%d", node->data.lex.num);
+            break;
+        default:
+            printf ("ERROR\n");
+            break;
+    }
+    fprintf (dotfile, "\"];\n");
+}
+
+void DtSetNodes (FILE *dotfile, struct node_t *top)
+{
+    DtSetNode (dotfile, top);
+    if (top->left)
+        DtSetNodes (dotfile, top->left);
+    if (top->right)
+        DtSetNodes (dotfile, top->right);
+}
+
+void DtSetDependencies (FILE *dotfile, struct node_t *top)
+{
+    if (top->left)
+    {
+        fprintf (dotfile, "Node%p->Node%p\n", top, top->left);
+        DtSetDependencies (dotfile, top->left);
+    }
+    if (top->right)
+    {
+        fprintf (dotfile, "Node%p->Node%p\n", top, top->right);
+        DtSetDependencies (dotfile, top->right);
+    }
+    fprintf (dotfile, "\n");
+}
+
+void DtEnd (FILE* dotfile)
+{
+    fprintf (dotfile, "}\n");
+} 
+
---
+++ lexer_and_parser/dump_tree/dump_tree.h
@@ -0,0 +1,6 @@
+struct node_t;
+void DtStart (FILE* dotfile);
+void DtSetNode (FILE* dotfile, struct node_t *node);
+void DtSetDependencies (FILE *dotfile, struct node_t *top);
+void DtEnd (FILE* dotfile);
+void DtSetNodes (FILE *dotfile, struct node_t *top);
